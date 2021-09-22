0c2500bcc685e0bdb892ba59110b60cf5e244d2c
avirup-chowdhury <43576491+avirup-chowdhury@users.noreply.github.com>
2021-09-22 12:47:47 +0100

    Update R script
    Added initial PCA, k-means, hierarchical clustering analyses.

--- heart_disease_clustering_script.R
+++ heart_disease_clustering_script.R
@@ -42,3 +42,63 @@ scaled <- scale(cropped) # To normalise data
 summary(scaled)
 
 rm(patients, cropped)
+
+# PCA ----
+pca_model <- prcomp(scaled, scale = FALSE)
+
+# Summarise variance per PC and plot
+components <- tibble(PC = c(1:10), 
+                     var = pca_model$sdev^2/sum(pca_model$sdev^2))
+
+ggplot(components, aes(x = PC, y = var)) + geom_point() + geom_line()
+
+biplot(pca_model)
+
+rm(components)
+
+# k-means ----
+sumsq <- data.frame(matrix(data = NA_integer_, nrow = 15, ncol = 2))
+
+# Identify best number of clusters
+set.seed(42)
+
+for (i in 1:15) {
+  output <- kmeans(scaled, centers = i, nstart = 20)
+  sumsq[i, ] <- c(i, output$tot.withinss)
+}
+
+ggplot(sumsq, aes(x = X1, y = X2)) + 
+  geom_line() + geom_point() +
+  labs(title = "Scree plot (k-means)") +
+  xlab("Number of clusters") +
+  ylab("Total within-cluster sum of squares")
+
+# Build model with 5 clusters
+set.seed(42)
+km_model <- kmeans(scaled, centers = 5, nstart = 20)
+
+clustered <- bind_cols(scaled, km_cluster = km_model$cluster)
+
+ggplot(clustered, aes(x = age, y = cholesterol, col = as.factor(km_cluster))) + 
+  geom_point()
+
+rm(sumsq, i, output)
+
+# Hierarchical clustering ----
+hier_model <- hclust(dist(scaled[-638, ]), method = "complete")
+# 638 is in their own cluster, hence omitted for now
+
+plot(hier_model) 
+
+# Get cluster assignments based on number of selected clusters
+hcm_cut <- cutree(hier_model, k = 5)
+
+clustered <- bind_cols(clustered, 
+                       
+                       # Fill patient 638 with result "NA"
+                       hm_cluster = c(hcm_cut[1:637], 
+                                      NA, 
+                                      hcm_cut[638:length(hcm_cut)]))
+
+# Comparing methods ----
+table(clustered$km_cluster, clustered$hm_cluster)
